c0a49cba206cf024496a8a28cd67a653ae22e082
Robert Chan <robertkhchan@gmail.com>
2016-04-19 00:51:51 -0400

    Fixed unit test for encounters.

--- test/controllers/encounters_controller_test.rb
+++ test/controllers/encounters_controller_test.rb
@@ -6,37 +6,37 @@ class EncountersControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
-    get :new
+    get :new, patient_id: @encounter.patient
     assert_response :success
   end
 
   test "should create encounter" do
     assert_difference('Encounter.count') do
-      post :create, encounter: { visit_number: @encounter.visit_number, admitted_at: @encounter.admitted_at, discharged_at: @encounter.discharged_at, location: @encounter.location, room: @encounter.room, bed: @encounter.bed }
+      post :create, patient_id: @encounter.patient, encounter: { visit_number: @encounter.visit_number, admitted_at: @encounter.admitted_at, discharged_at: @encounter.discharged_at, location: @encounter.location, room: @encounter.room, bed: @encounter.bed }
     end
 
     assert_redirected_to patient_path(@encounter.patient)
   end
 
   test "should show encounter" do
-    get :show, id: @encounter
+    get :show, patient_id: @encounter.patient, id: @encounter
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @encounter
+    get :edit, patient_id: @encounter.patient, id: @encounter
     assert_response :success
   end
 
   test "should update encounter" do
-    patch :update, id: @encounter, encounter: { visit_number: @encounter.visit_number, admitted_at: @encounter.admitted_at, discharged_at: @encounter.discharged_at, location: @encounter.location, room: @encounter.room, bed: @encounter.bed }
+    patch :update, patient_id: @encounter.patient, id: @encounter, encounter: { visit_number: @encounter.visit_number, admitted_at: @encounter.admitted_at, discharged_at: @encounter.discharged_at, location: @encounter.location, room: @encounter.room, bed: @encounter.bed }
     assert_redirected_to patient_path(@encounter.patient)
   end
 
   test "should destroy encounter" do
     @patient = @encounter.patient
     assert_difference('Encounter.count', -1) do
-      delete :destroy, id: @encounter
+      delete :destroy, patient_id: @encounter.patient, id: @encounter
     end
 
     assert_redirected_to patient_path(@patient)
